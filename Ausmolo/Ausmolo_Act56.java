a9d610e409bff636e2594b5a8ebe2aa4d9d74470
Renz Ryan Ausmolo <72122037+AusmoloRR@users.noreply.github.com>
2021-02-11 10:19:30 +0800

    Add files via upload

---
+++ Ausmolo/Ausmolo_Act56.java
@@ -0,0 +1,33 @@
+
+package ausmolo_act5.pkg6;
+
+public class Ausmolo_Act56 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++)
+        {
+            int n = 5;
+   
+            for (int j = 1; j<= n - i; j++) 
+            { 
+            System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--)
+            {
+                System.out.print(k);
+            }
+            for (int l = 2; l <= i; l++)
+            { 
+            System.out.print(l); 
+            } 
+            System.out.println(); 
+            } 
+            for (int i = 3; i >= 1; i--)
+        {
+            int n = 3;
+            System.out.println();
+        }
+       
+    }
+    }
+    
