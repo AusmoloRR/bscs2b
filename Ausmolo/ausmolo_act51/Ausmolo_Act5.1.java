680417351e9718e828120e66fda89d94f9f342bf
Renz Ryan Ausmolo <72122037+AusmoloRR@users.noreply.github.com>
2021-02-11 10:16:39 +0800

    Add files via upload

---
+++ Ausmolo/ausmolo_act51/Ausmolo_Act5.1.java
@@ -0,0 +1,21 @@
+
+package ausmolo_act51;
+
+public class Ausmolo_Act51 {
+
+    public static void main(String[] args) {
+        {
+ int i,j,k;
+for(i=1; i<=4; i++)
+{
+for(j=1; j<=10; j++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+    }
+    
+}
+
---
+++ Ausmolo/ausmolo_act51/Ausmolo_Act51.java
@@ -0,0 +1,20 @@
+
+package ausmolo_act51;
+
+public class Ausmolo_Act5.1 {
+
+    public static void main(String[] args) {
+        {
+ int i,j,k;
+for(i=1; i<=4; i++)
+{
+for(j=1; j<=10; j++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+    }
+    
+}
