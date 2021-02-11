5967e6094bf597d2b3f5de255edf206fa8c6f7a6
Renz Ryan Ausmolo <72122037+AusmoloRR@users.noreply.github.com>
2021-02-11 10:14:58 +0800

    Add files via upload

---
+++ Ausmolo/Ausmolo.java
@@ -0,0 +1,20 @@
+
+package ausmolo;
+
+class Ausmolo {
+
+    public static void main(String[] args) {
+    String yourName;
+          yourName = ("Renz Ryan");       
+        int Num = 10;
+        char Let = 'a';
+        Boolean a =true;
+        
+      
+        System.out.println(Num);
+        System.out.println(Let);
+        System.out.println(a);
+        System.out.println("Hello "+yourName);
+    }
+    
+}
